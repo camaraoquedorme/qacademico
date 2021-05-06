d42f82e38c1d1e607168a262877755e1a2d649cd
Ronaldo Borges Guimarães <ronaldo.bguimaraes@outlook.com>
2021-05-06 13:14:57 -0400

    jQquery checkbox

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
-function mostrarSenha(check) {
+// seleciona o input de senha
+const txtSenha = $("#txtSenha");
 
-  const type = check.checked ? "text" : "password";
+// adicona evento de onchange no checkbox
+$("#showSenha").on("change", function (e) {
 
-  $("#txtSenha").attr("type", type);
-}
+  const type = e.target.checked ? "text" : "password";
+
+  // troca o type do input da senha
+  txtSenha.attr("type", type);
+});
